86fdba8b47771701751c427ca0e4152cde0a4cf3
Dominik Czesak <czesak.dominik99@gmail.com>
2022-01-07 20:28:52 +0100

    Made login panel

---
+++ src/main/java/com/example/bankaccount/LoginController.java
@@ -0,0 +1,66 @@
+package com.example.bankaccount;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private AnchorPane parent;
+
+    @FXML
+    private HBox top;
+
+    @FXML
+    private Pane content;
+
+    private double xOffSet = 0;
+    private double yOffSet = 0;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    private void makeStageDragable() {
+        parent.setOnMousePressed((event) -> {
+            xOffSet = event.getSceneX();
+            yOffSet = event.getSceneY();
+        });
+        parent.setOnMouseDragged((event) -> {
+            Main.stage.setX(event.getScreenX() - xOffSet);
+            Main.stage.setY(event.getScreenY() - yOffSet);
+            Main.stage.setOpacity(0.8f);
+        });
+        parent.setOnDragDone((event) -> {
+            Main.stage.setOpacity(1.0f);
+        });
+        parent.setOnMouseReleased((event) -> {
+            Main.stage.setOpacity(1.0f);
+        });
+    }
+
+    @FXML
+    private void minimize_stage(MouseEvent event) {
+        Main.stage.setIconified(true);
+    }
+
+    @FXML
+    private void close_app(MouseEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    private void handle_login(MouseEvent event) {
+
+    }
+}
--- src/main/java/com/example/bankaccount/Main.java
+++ src/main/java/com/example/bankaccount/Main.java
@@ -2,18 +2,25 @@ package com.example.bankaccount;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
+
+    public static Stage stage = null;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 640);
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        this.stage = stage;
         stage.show();
     }
 
