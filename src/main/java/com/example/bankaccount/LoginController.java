17ce6e2b0ea8d46a077ca0529afc171828845663
Dominik Czesak <czesak.dominik99@gmail.com>
2022-01-09 16:18:38 +0100

    Fixed bug with login button

--- src/main/java/com/example/bankaccount/LoginController.java
+++ src/main/java/com/example/bankaccount/LoginController.java
@@ -1,9 +1,12 @@
 package com.example.bankaccount;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -11,6 +14,7 @@ import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
@@ -23,6 +27,9 @@ public class LoginController implements Initializable {
 
     @FXML
     private Pane content;
+
+    @FXML
+    private Button login_button;
 
     private double xOffSet = 0;
     private double yOffSet = 0;
@@ -62,10 +69,10 @@ public class LoginController implements Initializable {
     }
 
     @FXML
-    private void handle_login(MouseEvent event) throws IOException {
-//        Parent menu = FXMLLoader.load(getClass().getResource("main-window-view.fxml"));
-//        content.getChildren().removeAll();
-//        content.getChildren().setAll(menu);
+    private void handle_login(ActionEvent event) throws IOException {
+        Parent menu = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-window-view.fxml")));
+        content.getChildren().removeAll();
+        content.getChildren().setAll(menu);
         System.out.println("Button works");
     }
 }
